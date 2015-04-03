c41f46df975293259fe1f8932634e1dd1d9317d0
Amanda.W <manderlou@me.com>
2015-04-03 08:51:52 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,305 @@
+
+<!doctype html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-48232520-1', 'u23developmentteam.com');
+            ga('send', 'pageview');
+
+        </script>
+
+
+        <title>U23 Development Team</title>
+        <meta name="description" content="">
+
+        <!-- Mobile viewport optimized: h5bp.com/viewport -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+
+        <link rel="stylesheet" media="screen" href="css/superfish.css"/>
+        <link rel="stylesheet" href="css/nivo-slider.css" media="all"/>
+        <link rel="stylesheet" href="css/tweet.css" media="all"/>
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" media="all" href="css/mediaqueries.css"/>
+        <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../img/touch-icon-ipad-precomposed.png"/>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="img/favicon1.ico" type="image/x-icon">
+        <!-- All JavaScript at the bottom, except this Modernizr build.
+           Modernizr enables HTML5 elements & feature detects for optimal performance.
+           Create your own custom Modernizr build: www.modernizr.com/download/ -->
+        <script src="js/modernizr-2.5.3.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/jquery-1.7.1.min.js"><\/script>')</script>
+        <script type="text/javascript" language="javascript" src="js/jquery.carouFredSel-6.2.1-packed.js"></script>
+        <script type="text/javascript" language="javascript">
+            $(window).load(function () {
+                var foo = $('#foo4');
+
+                //	Responsive layout, resizing the items
+                if(foo.length) {
+                    foo.carouFredSel({
+                        responsive: true,
+                        timeoutDuration: 3000,
+                        width: '100%',
+                        scroll: 1,
+                        items: {
+                            width: 220,
+                            visible: {
+                                min: 2,
+                                max: 2
+                            }
+                        }
+                    });
+                }
+
+            });
+        </script>
+
+    </head>
+    <body><!-- HEADER -->
+<header>
+
+    <!-- header wrapper -->
+    <div class="wrapper cf">
+        <div id="logo">
+            <img src="img/oakley-h.png" alt="Oakley" class="u23">
+            <a href="/"><img src="img/mellow-johnnys.png" alt="Mellow Johnny's Bike Shop" class="ph"/></a>
+            <a href="/"><img src="img/u23.png" alt="U23 Development Team" class="ph"/></a>
+            <img src="img/castelli.png" alt="Castelli" class="ph">
+            <img src="img/phlogo.png" alt="Pedal Hard Training" class="ph">
+        </div>
+
+    </div>
+    <!-- ENDS header wrapper -->
+
+
+    <!-- nav -->
+    <nav class="cf">
+        <div class="wrapper cf">
+            <ul class="icons">
+
+                <li>
+                    <a class="icon icon-facebook" href="https://www.facebook.com/pages/U23developmentteam/604354686276789" target="_blank"></a>
+                </li>
+                <li>
+                    <a class="icon icon-twitter" href="https://twitter.com/U23DevTeam" target="_blank"></a>
+                </li>
+                <li>
+                    <a class="fa fa-youtube-square" href="https://www.youtube.com/channel/UCT9fL7_PBky1BhRkQ6DwJ9A" target="_blank"></a>
+                </li>
+
+
+            </ul>
+
+            <ul id="nav" class="sf-menu">
+                <li><a href="/" class="home">Home</a></li>
+                <li><a href="goals.php" class="goals">Team Goals</a></li>
+                <li><a href="#" class="about">Team Bios</a>
+                    <ul>
+                        <li><a href="carson1.php">Carson Lange</a></li>
+                        <li><a href="robby.php">Robby Ketterhagen</a></li>
+                        <li><a href="tc.php">TC Porterfield</a></li>
+                        <li><a href="john-paul-blanton.php">John Paul Blanton</a></li>
+                        <li><a href="isaac-porterfield.php">Isaac Porterfield</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" class="schedule">Race Results</a>
+                    <ul>
+                        <li>
+                            <a href="race-schedule.php" class="schedule">2014 Results</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li><a href="sponsors.php" class="join">Sponsors</a></li>
+                <li><a href="contactus.php" class="contact">Contact</a></li>
+            </ul>
+
+            <div id="combo-holder"></div>
+        </div>
+    </nav>
+    <!-- ends nav -->
+
+</header>
+<!-- ENDS HEADER -->
+    <!-- MAIN -->
+    <div role="main" id="main">
+        <div class="wrapper">
+
+            <!-- SLIDER -->
+            <div class="slider-wrapper theme-default theme-project theme-home">
+                <div class="ribbon"></div>
+                <div id="slider" class="nivoSlider">
+                    <img src="img/slideimg.jpg" alt="" title="#Slide1"/>
+                    <img src="img/slideimg2.jpg" alt="" title="#Slide2"/>
+                    <img src="img/slideimg3.jpg" alt="" title="#Slide3"/>
+                    <img src="img/jp-banner.jpg" alt="" title="#Slide4"/>
+                    <img src="img/isaac-banner.jpg" alt="" title="#Slide5"/>
+                </div>
+                <div id="Slide1" class="nivo-html-caption">
+                    <h1>Carson Lange</h1><a href="carson1.php">Check out his bio<i class="icon-caret-right"></i></a>
+                </div>
+                <div id="Slide2" class="nivo-html-caption">
+                    <h1>Robby Ketterhagen</h1><a href="robby.php">Check out his bio<i class="icon-caret-right"></i></a>
+                </div>
+                <div id="Slide3" class="nivo-html-caption">
+                    <h1>TC Porterfield</h1><a href="tc.php">Check out his bio<i class="icon-caret-right"></i></a>
+                </div>
+                <div id="Slide4" class="nivo-html-caption">
+                    <h1>John Paul Blanton</h1><a href="john-paul-blanton.php">Check out his bio<i class="icon-caret-right"></i></a>
+                </div>
+                <div id="Slide5" class="nivo-html-caption">
+                    <h1>Isaac Porterfield</h1><a href="isaac-porterfield.php">Check out his bio<i class="icon-caret-right"></i></a>
+                </div>
+
+            </div>
+            <!-- ENDS SLIDER -->
+
+
+            <!-- headline -->
+
+            <!-- ENDS headline -->
+
+
+            <!-- Feature -->
+            <div class="page-content">
+
+                <!-- entry-content -->
+                <div class="entry-content cf">
+                    <div class="regpage">
+                        <h2>U23 Development Team</h2>
+                        <img src="img/team.jpg" class="team">
+
+                        <h3>Our Mission</h3>
+
+                        <p>We strive to develop an amateur bicycle race team with a focus on teamwork and success as a team. Our intent is to be successful in all disciplines of cycling and to represent our sponsors with pride and confidence.</p>
+
+                        <h3>Our Impassioned Vision</h3>
+
+                        <p>The vision of the U23 Organization is to make Texas Cycling fun and sustainable at the highest levels for all our team mates and to produce a positive and high level of exposure for our sponsors. Competing is our passion and drives us to race at the most elite levels</p>
+
+                        <h3>Team Objective</h3>
+
+                        <p>It has been our goal to not only become ambassadors for the sport of cycling, but it has also been our passion to give back to our beloved past time. With that we proudly announce the development of U23 cycling team in partnership with Pedal Hard Training for the 2014 season. U23 is a fledgling class of passionate young cyclists that have dreams and aspirations to become the best in their sport. One of our main goals is to help develop these young riders and hone them into premier athletes as well as exceptional young men. With the help of our gracious sponsors our objective is to prepare these young men for local and amateur races that will one day walk away with valuable life skills being learnt.</p>
+
+                        <div class="caroufredsel_wrapper">
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+
+                    <div class="headline">
+    Contribute to the U23 Development Team
+    <li><a href="donate1.php" class="link-button green">Donate Now!</a></li>
+</div>
+<div class="one-fourth">
+    <h4 class="heading">Sponsors</h4>
+
+    <div class="image_carousel">
+        <div id="foo4">
+            <a href="http://pedalhard.com/" target="_blank"><img src="img/ph2.png"/></a>
+            <a href="http://audiencegeneration.com/" target="_blank"><img src="img/ag.png"/></a>
+            <a href="http://www.oakley.com/" target="_blank"><img src="img/oakley.png"/></a>
+            <a href="http://www.castelli-cycling.com/" target="_blank"><img src="img/castelli-s.png"/></a>
+            <a href="http://www.ciaocielocycling.com/" target="_blank"><img src="img/ccbig.png"></a>
+            <a href="http://www.howrybreen.com/" target="_blank"><img src="img/hbh.png"/></a>
+            <a href="http://www.rolwheels.com/" target="_blank"><img src="img/rol1.png"/></a>
+            <a href="http://www.innersunchiropractic.com/" target="_blank"><img src="img/scs.png"/></a>
+            <a href="http://www.mellowjohnnys.com/" target="_blank"><img src="img/mjlogo.png"/></a>
+            <a href="http://www.www.voodooracing.net/" target="_blank"><img src="img/vd2.png"/></a>
+            <a href="http://www.hollandracing.net/" target="_blank"><img src="img/hr.png"/></a>
+            <a href="http://www.conti-online.com/www/bicycle_de_en/" target="_blank"><img src="img/continental-tire.png"/></a>
+            <a href="http://www.giro.com" target="_blank"><img src="img/giro.png"/></a>
+            <a href="http://www.reliv.com" target="_blank"><img src="img/reliv.png"/></a>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</div>
+<!-- ENDS feature -->                </div>
+
+            </div>
+        </div>
+        <!-- ENDS MAIN -->
+    </div>
+<div class="push"></div>
+<footer class="footer">
+
+    <!-- wrapper -->
+    <div class="wrapper cf">
+
+
+        <!-- social -->
+
+        <!-- ENDS social -->
+
+
+        <!-- widgets -->
+        <!-- ENDS widgets -->
+
+
+        <div id="footer-bottom"><p class="copyright">&copy; 2015 U23 Development Team</p>
+
+            <p class="powered">Powered by
+                <span><a href="http://audiencegeneration.com/" target="_blank"><img src="img/favicon.png"></a></span>
+        </div>
+
+
+    </div>
+    <!-- ENDS wrapper -->
+
+
+</footer>
+
+
+<!-- JavaScript at the bottom for fast page loading -->
+
+<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+
+
+<!-- scripts concatenated and minified via build script -->
+<script src="js/custom.js"></script>
+<script type="text/javascript" language="javascript" src="js/jquery.carouFredSel-6.2.1-packed.js"></script>
+
+<!-- optionally include helper plugins -->
+<script type="text/javascript" language="javascript" src="js/jquery.mousewheel.min.js"></script>
+<script type="text/javascript" language="javascript" src="js/jquery.touchSwipe.min.js"></script>
+<script type="text/javascript" language="javascript" src="js/jquery.transit.min.js"></script>
+<script type="text/javascript" language="javascript" src="js/jquery.ba-throttle-debounce.min.js"></script>
+<!-- superfish -->
+<script src="js/superfish-1.4.8/js/hoverIntent.js"></script>
+<script src="js/superfish-1.4.8/js/superfish.js"></script>
+<script src="js/superfish-1.4.8/js/supersubs.js"></script>
+<!-- ENDS superfish -->
+
+<script src="js/tweet/jquery.tweet.js"></script>
+<script src="js/jquery.isotope.min.js"></script>
+<script src="js/jquery.nivo.slider.js"></script>
+<script src="js/css3-mediaqueries.js"></script>
+<script src="js/tabs.js"></script>
+<script src="js/poshytip-1.1/src/jquery.poshytip.min.js"></script>
+<!-- end scripts -->
+
+</body>
+</html>
